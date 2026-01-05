5fe0936e9b3701e8b051f7a80b217ca9797881da
Muhammad Bilal Madni <117742850+MuhammadBilalpro@users.noreply.github.com>
2026-01-05 21:17:21 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,10 +2,31 @@
     <div class="footer-content">
         <div class="footer-col">
             <h4>Contact Us</h4>
-            <p><i class="fas fa-envelope"></i> email@motortrade-referrals.co.uk</p>
-            <p><i class="fas fa-phone"></i> 0800 123 4567</p>
+            
+            <p>
+                <i class="fas fa-map-marker-alt"></i> 
+                <a href="https://www.google.com/maps/search/?api=1&query=Street15,+Leicester+LE1+1AA" target="_blank" style="color: inherit; text-decoration: none;">
+                    Street15, Leicester LE1 1AA
+                </a>
+            </p>
+
+            <p>
+                <i class="fas fa-envelope"></i> 
+                <a href="mailto:james@motortradeinsurancesra.co.uk" style="color: inherit; text-decoration: none;">
+                    james@motortradeinsurancesra.co.uk
+                </a>
+            </p>
+
+            <p>
+                <i class="fas fa-phone"></i> 
+                <a href="tel:01183701701" style="color: inherit; text-decoration: none;">
+                    0118 370 1701
+                </a>
+            </p>
+
             <p>Mon - Fri: 9:00 AM - 6:00 PM</p>
         </div>
+
         <div class="footer-col">
             <h4>Quick Links</h4>
             <ul>
@@ -14,6 +35,7 @@
                 <li><a href="privacy.php">Privacy Policy</a></li>
             </ul>
         </div>
+
         <div class="footer-col">
             <h4>Important Notice</h4>
             <p style="font-size: 0.9rem; color: #ccc;">
@@ -21,6 +43,7 @@
             </p>
         </div>
     </div>
+    
     <div class="copyright">
         &copy; <?php echo date("Y"); ?> Motor Trade Referral Specialists. All Rights Reserved.
     </div>
