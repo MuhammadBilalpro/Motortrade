037335ec0307fdeb67dd52b02d2088fc49df03f7
Muhammad Bilal Madni <117742850+MuhammadBilalpro@users.noreply.github.com>
2026-01-22 21:29:15 +0500

    Update header.php

--- header.php
+++ header.php
@@ -164,6 +164,14 @@ $seoData = array(
     <!-- Preconnect for performance -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://cdnjs.cloudflare.com">
+    <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "v5iipa9aus");
+</script>
+    
 </head>
 <body>
 
